52720e076e14ed3a1c80dc782501fb55a942a783
backalleyblitz <134005418+backalleyblitz@users.noreply.github.com>
2024-07-29 03:39:38 +0300

    Замена хендлеров.
    Добавлен хендлер /start, хендлеры /help, /rules временно заменены на хендлер отправки сообщений.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 from aiogram import Dispatcher
-from bot import create_tables, bot, dp
-from send import message_worker, handle_rules, handle_help
+from bot import create_tables, bot, dp, handle_start
+from send import message_worker, handle_message
 
 async def on_startup(dispatcher):
     await create_tables()
